6264e1bc8df435a89aa7c3f441e87f57201638cf
cobo0120 <cobo94251@gmail.com>
2024-01-21 13:32:50 +0900

    20230121

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -24,68 +24,40 @@ class CalculateController extends Controller
 
 
 public function data_building(Request $request){
-  dd($request);
-    // 建築構造を値で取得する（建築構造はvalue数値で$requestに渡される）
-   $building_structure = $request->input('building_structure');
-   switch ($building_structure) {
-       case 1:
-           $columnName = 'src_price';
-           break;
-       case 2:
-           $columnName = 'rc_price';
-           break;
-       case 3:
-           $columnName = 'sc_price';
-           break;
-       case 4:
-           $columnName = 'lgs_price';
-           break;
-       case 5:
-           $columnName = 'wood_price';
-           break;
-   }
-
-    // 年と構造に基づいてデータベースから建設費を取得
-   $cost = DB::table('buildings')
-       ->where('building_age')
-       ->where($columnName, '<>', '') // 空でない値を選択
-       ->first()
-       ->$columnName; // ダイナミックなカラム名を使用
-
-   // 標準建築費をJSON形式でレスポンスとして返す
-   return response()->json
-   ([
-    'constructionCost' => $cost,
-    ]);
+    
+    // 建築構造を値で取得する（建築構造はvalue数値で$requestに渡されるはず）
+    $building_structure = $request->building_structure;
+    switch ($building_structure) {
+        case "1":
+            $columnName = 'src_price';
+            break;
+        case "2":
+            $columnName = 'rc_price';
+            break;
+        case "3":
+            $columnName = 'sc_price';
+            break;
+        case "4":
+            $columnName = 'lgs_price';
+            break;
+        case "5":
+            $columnName = 'wood_price';
+            break;
     }
+
+    // 年と構造に基づいてデータベースから標準建設費を取得
+    $construction_cost = DB::table('buildings')
+    ->where('building_age', $request->built_year)
+    ->whereRaw("{$columnName} IS NOT NULL")
+    ->first();
+
+    // 標準建築費をJSON形式でレスポンスとして返す
+    return response()->json([
+        'constructionCost' => $construction_cost,
+    ]);
 }
 
 
 
-// public function data_destination(Request $request)
-// {
- 
-//     // データを取得（1〜5件取ってくる）各ページごとに最初の行をスキップしてデータを取得するように設定
-//     $users = User::offset(($request->page-1)*3)->limit(3)->get();
-//     foreach($users as $user){
-//         $user['department'] = $user->department()->get();
-//     }
-//     $allUsers=User::all();
-    
-//     // データ数を取得
-//     // $dataCount = count($allUsers);
-//     // ページ番号の最大値を取得
-//     $pageMax = ceil(count($allUsers) / 3);
-//     // // ページ番号を生成するための配列を作成
-//     // $pageNumbers = [];
-//     // // ページ番号を生成する
-//     // for ($i = 1; $i <= $pageMax; $i++) {
-//     // $pageNumbers[] = $i;
-//     //   }
 
-//     // 部署名を表示させるには多次元連想配列を使う？
-//     return response()->json([
-//         'users' => $users,  
-//         'pageMax' => $pageMax,]);
-    
-// }
+}
--- public/js/building.js
+++ public/js/building.js
@@ -1,148 +1,28 @@
-let changeTimer = null;
-
 $('#building_age').on('input', function() {
- if (changeTimer !== null) clearTimeout(changeTimer);
- changeTimer = setTimeout(function() {
-  // 現在の年数を取得
-   let current_year = new Date().getFullYear();
-  // ビューのcalc1にある築年数を取得する
-   let age_of_building = $('#building_age').val();
-  // データベースの値（西暦）を取得のため{（現在の西暦ー築年数＝建築築年（西暦））}
-   let built_year = current_year - age_of_building;
-  //  ビューのcalc1にあるbuilding_structureのvalueを条件に算出する
-   let building_structure = $('#building_structure').val();
-  //  建物の年数（西暦）と建築構造を条件にデータベースから値を取得する
-   
-   $.ajax({
-       type: "POST",
-       url: "../data_building",
-       async: true,
-       dataType: 'json',
-       data: { 
-        building_structure: building_structure, 
-        built_year: built_year 
-        },//ここはサーバーに贈りたい情報。
-   })
-   .done((data) => {
-    // もしデータベース内にデータがない場合（例えば、築年数１〜3年はデータベースにないor築年数が古くてない）
-      if (typeof data !== 'undefined' && data !== null) {
-          $('#construction_cost').val(data.constructionCost);
-      } 
-      else {
-          // もし建築年（西暦）が3年よりも小さい場合
-          if (building_age >= 3 ) {
-              // データベースから該当の建築構造の古い数値を取得し
-              $('#construction_cost').val(data.constructionCost);
-              // それ以外ならば
-          } else {
-              // データベースから該当の建築構造の最新数値を取得
-             $('#construction_cost').val(data.constructionCost);
-          }
+    // 現在の年を取得
+    let current_year = new Date().getFullYear();
+    // ビューのcalc1のvalueにある築年数を取得する
+    let building_age = $('#building_age').val();
+    // データベースの値（西暦）を取得のため{（現在の西暦ー築年数＝建築築年（西暦））}
+    let built_year = current_year - building_age;
+    //  ビューのcalc1にあるbuilding_structureのvalueを条件に算出する
+    let building_structure = $('#building_structure').val();
 
-          $.ajax({
-              type: "POST",
-              url: "../data_building",
-              async: true,
-              dataType: 'json',
-              data: { 
-                building_structure: building_structure, 
-                built_year: built_year 
-                },//ここはサーバーに贈りたい情報。
-          })
-          .done((data) => {
-            // calc1のビューでid=construction_costの箇所に入れる
-              $('#construction_cost').val(data.constructionCost);
-          })
-          .fail((error) => {
-              alert('通信失敗');
-          });
-      }
-  })
-   .fail((error) => {
-       alert('通信失敗');
-   });
-   changeTimer = null;
- }, 300);
+    $.ajax({
+        type: "POST",
+        url: "../data_building",
+        async: true,
+        dataType: 'json',
+        data: {
+            built_year: built_year,
+            building_structure: building_structure,
+        }
+    })
+    .done((data) => {
+        // calc1のビューでid=construction_costの箇所に入れる
+        $('#construction_cost').val(data.constructionCost);
+    })
+    .fail((error) => {
+        alert('通信失敗');
+    });
 });
-
-
-// let changeTimer = null;
-
-// $('#building_age').on('input', function() {
-//  if (changeTimer !== null) clearTimeout(changeTimer);
-//  changeTimer = setTimeout(function() {
-//      let current_year = new Date().getFullYear();
-//      let age_of_building = $('#building_age').val();
-//      let built_year = current_year - age_of_building;
-     
-//      $.ajax({
-//          type: "GET",
-//          url: "../data_building",
-//          async: true,
-//          dataType: 'json',
-//          data: { page: 1 },
-//      })
-//      .done((data) => {
-//          $('#construction_cost').val(data.constructionCost);
-//      })
-//      .fail((error) => {
-//          alert('通信失敗');
-//      });
-//      changeTimer = null;
-//  }, 300);
-// });
-
-
-
-
-// // calc1からidのbuilding_ageの数値が変化したら非同期処理を開始する
-// $('#building_age').on('change', () => {
-//  $.ajax({
-//    type: "GET",
-//    url: "../data_building",
-//    async: true,
-//    dataType: 'json',
-//    data: { page: 1 },
-//  })
-//  .done((data) => {
-
-//   // 標準建築価格を築年数ごとに変更させて#building_ageのinput欄に表示させる
-//    const constructionCost = $("#construction_cost");
-//    constructionCost.val(data.constructionCost);
-//  })
-//  .fail((error) => {
-//    alert('通信失敗');
-//  });
-// });
-// $('.building').on('click', () => {
-//  $.ajax({
-//    type: "GET",
-//    url: "../data_destination",
-//    async: true,
-//    dataType: 'json',
-//    data: { page: 1 },
-//  })
-//  .done((data) => {
-//    const constructionCost = $("#construction_cost");
-//    constructionCost.val(data.constructionCost);
-//  })
-//  .fail((error) => {
-//    alert('通信失敗');
-//  });
-// });
-
-// $(document).on('click', () => {
-//  $("#building_structure").on('change', () => {
-//    let structure = $("#building_structure").val();
-//    let age = $("#building_age").val();
-//    updateData(structure, age);
-//  });
-
-//  $("#building_age").on('change', () => {
-//    let structure = $("#building_structure").val();
-//    let age = $("#building_age").val();
-//    updateData(structure, age);
-//  });
-// });
-
-// function updateData(structure, age) {
